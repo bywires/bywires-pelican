570ed5dbadf6abcff09b351f313062e9bfed97bf
Bob McKee <bmckee@bywires.com>
2012-12-28 12:38:43 -0500

    started theme, webassets and sass integration started

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,19 +9,13 @@ TIMEZONE = 'America/New_York'
 
 DEFAULT_LANG = u'en'
 
-# Blogroll
-LINKS =  (('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
-          ('Python.org', 'http://python.org'),
-          ('Jinja2', 'http://jinja.pocoo.org'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
 DEFAULT_PAGINATION = 5
 
 ARTICLE_URL = 'articles/{category}/{slug}/'
 ARTICLE_SAVE_AS = 'articles/{category}/{slug}/index.html'
 
+PLUGINS = ['pelican.plugins.assets']
+
 THEME = 'themes/bywires'
+
+WEBASSETS = True
